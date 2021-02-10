72ca3b0283ec77263ce820aa8ce0d2d0adf152e2
CyganFx <duman070601@gmail.com>
2021-02-10 15:26:02 +0600

    refactor

--- src/main/java/com/example/bankSpring/threads/Client.java
+++ src/main/java/com/example/bankSpring/threads/Client.java
@@ -9,23 +9,18 @@ import java.net.Socket;
 public class Client extends Thread {
 
     public String processComplaintAndGetResponse(String complaintMessage) throws IOException, ClassNotFoundException, InterruptedException {
-        //get the localhost IP address, if server is running on some other IP, you need to use that
         InetAddress host = InetAddress.getLocalHost();
         Socket socket;
         ObjectInputStream ois;
         ObjectOutputStream oos;
-        //establish socket connection to server
         socket = new Socket(host.getHostName(), 9876);
-        //write to socket using ObjectOutputStream
         oos = new ObjectOutputStream(socket.getOutputStream());
         oos.writeObject(complaintMessage);
         System.out.println("Sending request to Socket Server");
         Thread.sleep(2000);
-        //read the server response message
         ois = new ObjectInputStream(socket.getInputStream());
         String message = (String) ois.readObject();
         System.out.println("Message: " + message);
-        //close resources
         ois.close();
         oos.close();
         return message;
